17bd0a48285260292cd422709f31be7756ff1782
MercyRima <47768911+MercyRima@users.noreply.github.com>
2019-05-09 19:11:24 +0300

    share a new file

---
+++ quiz.py
@@ -0,0 +1,61 @@
+def division (division):
+	division = []
+	for number in numbers:
+		x=/3
+		division.append(x)
+		print(division)
+
+def sorted():
+	list1=['apple','banana','mango']
+	list2=['avocado','peach','orange']
+	list3=['pineapple','lemon']
+	list4=list1+list2+list3
+	list4.sort()
+	print(list4)
+
+	
+	
+def divisible_by_three(n):
+	x = division(range(1,n+1/3))
+	print(x)
+
+
+def nest(c):
+	for v in x:
+		for a in v:
+			print (a)
+
+
+def smallest():
+	list = ([1,3,6,8,2,4,5.7]) 
+	k = min(list)
+	print(k)
+
+
+def duplicate(list):
+	list = set ()
+	return list
+
+
+
+def square()
+	n = dict()
+	for x in range(149,153):
+		n[x]=x**2
+		print(n)
+
+
+
+def student():
+	student1={"name":"Eunice","age":19}
+	student2={"name":"Agnes","age":21}
+	student3={"name":"Teresa","age":18}
+	student4={"name":"Asha","age":22}
+	students=[student1,student2,student3,student4]
+	for student in students:
+		sms = "Hello {}, you were born in the year {}".format(student["name"],student["year"])
+		print(sms)
+
+
+
+		
